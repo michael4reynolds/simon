25b7f1c0f3c38379014248bedce94e914c7c1bc6
Michael Reynolds <michael4reynolds@gmail.com>
2017-10-30 07:36:51 -0400

    add sound

--- js/main.js
+++ js/main.js
@@ -15,19 +15,38 @@ const arrayIsEqual = (arr1, arr2) => {
   return JSON.stringify(arr1.slice(0, arr2.length)) === JSON.stringify(arr2)
 }
 
+class Sound {
+  constructor(src) {
+    this.sound = document.createElement("audio")
+    this.sound.src = src
+    this.sound.setAttribute("preload", "auto")
+    this.sound.setAttribute("controls", "none")
+    this.sound.style.display = "none"
+    document.body.appendChild(this.sound)
+  }
+
+  play() {
+    this.sound.play()
+  }
+
+  stop() {
+    this.sound.pause()
+  }
+}
+
 // Model
-const BLUE = {key: 0, button: document.querySelector('.blueBtn')}
-const YELLOW = {key: 1, button: document.querySelector('.yellowBtn')}
-const GREEN = {key: 2, button: document.querySelector('.greenBtn')}
-const RED = {key: 3, button: document.querySelector('.redBtn')}
+const BLUE = {key: 0, button: document.querySelector('.blueBtn'), tone: 'sounds/simonSound1.mp3'}
+const YELLOW = {key: 1, button: document.querySelector('.yellowBtn'), tone: 'sounds/simonSound2.mp3'}
+const GREEN = {key: 2, button: document.querySelector('.greenBtn'), tone: 'sounds/simonSound3.mp3'}
+const RED = {key: 3, button: document.querySelector('.redBtn'), tone: 'sounds/simonSound4.mp3'}
 
 const buttonsPads = [BLUE, YELLOW, GREEN, RED]
 const PRESS = 'press'
 const DELAY = 500
+let padSound
 let LISTENING = false
 let GAME_OVER = false
 let timer
-
 
 let notes = []
 let recalled = []
@@ -46,9 +65,12 @@ async function playNotes() {
   let play = async () => {
     for (let note of notes) {
       let button = buttonsPads[note].button
+      padSound = new Sound(buttonsPads[note].tone)
       button.classList.add(PRESS)
+      padSound.play()
       await sleep(DELAY)
       button.classList.remove(PRESS)
+      // padSound.stop()
       await sleep(DELAY)
     }
   }
@@ -76,11 +98,14 @@ buttonsPads.forEach(pad => {
   pad.button.onmousedown = () => {
     if (LISTENING) {
       pad.button.classList.add(PRESS)
+      padSound = new Sound(pad.tone)
+      padSound.play()
     }
   }
   pad.button.onmouseup = async () => {
     if (LISTENING) {
       pad.button.classList.remove(PRESS)
+      // padSound.stop()
       recalled.push(pad.key)
       if (timer) timer.cancel()
       if (!arrayIsEqual(notes, recalled)) {
